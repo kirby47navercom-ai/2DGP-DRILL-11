59c1bea328cf843f6d89b1f62d9d4d10361d3efb
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-28 15:23:31 +0900

    수업끝

---
+++ game_world.py
@@ -0,0 +1,26 @@
+world = [[],[]]
+
+def add_object(o,depth=0):
+    global world
+    world[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    else:
+        print("ㅇㅅㅇ 없어용:")
+
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
